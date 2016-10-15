d9bc0b4416a640e2cb395ba608522e306c133d8b
grammka <grammka@gmail.com>
2016-10-15 19:26:18 +0300

    fix compiling style sources

--- example/util/source.js
+++ example/util/source.js
@@ -9,7 +9,31 @@ var cwdPath     = process.cwd();
 var basePath    = path.join(cwdPath, '../');
 var inputPath   = path.join(basePath, './src');
 var pattern     = path.join(inputPath, '*/spinners/*');
+var extPattern  = path.join(inputPath, '*');
 var outputPath  = path.join(cwdPath, './build/source.js');
+
+
+var additionals = {};
+
+var types = glob.sync(extPattern).map(function(filePath) {
+  return {
+    path: filePath,
+    ext: filePath.match(/[^\/]+$/)[0]
+  };
+});
+
+types.forEach(function(type) {
+	var filePaths = glob.sync(type.path + '/_*');
+  
+  additionals[type.ext] = '';
+
+  for (var i = 0; i < filePaths.length; i++) {
+    var filePath    = filePaths[i];
+    var source      = fs.readFileSync(filePath, 'utf8');
+
+    additionals[type.ext] += source + '\n';
+  }
+});
 
 
 glob(pattern, {}, function(err, filePaths) {
@@ -21,16 +45,16 @@ glob(pattern, {}, function(err, filePaths) {
     var filePath    = filePaths[i];
     var sourceNum   = filePath.match(/\d+/)[0];
     var sourceExt   = filePath.match(/[^\.]+$/)[0];
-  
+
     var source      = fs.readFileSync(filePath, 'utf8');
     
     if (!output[`${sourceNum}`]) {
       output[`${sourceNum}`] = {}
     }
-  
-    output[`${sourceNum}`][sourceExt] = source;
+
+    output[`${sourceNum}`][sourceExt] = additionals[sourceExt] + source;
   }
-  
+
   var result = `var source = ${JSON.stringify(output, null, 2).replace(/\n$/, '')};\n`;
   
   fs.writeFile(outputPath, result, function(err) {
